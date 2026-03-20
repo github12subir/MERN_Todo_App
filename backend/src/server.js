c6392cecf8b594268c023e0bc554eecad9d20f6f
Subir Mondal <subir.m@stylopay.com>
2026-03-20 22:13:53 +0530

    feat(backend): add API routes for auth and tasks

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/tasks", require("./routes/taskRoutes"));
+
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
